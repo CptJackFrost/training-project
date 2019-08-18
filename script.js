d3a4415ae27b087e945f99c3c600b5d50e7c2afd
Ivan <blackout12@rambler.ru>
2019-08-18 16:41:59 +0500

    Started calculator application

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц:");
+let money = +prompt("Ваш бюджет на месяц:");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
@@ -10,12 +10,64 @@ let appData = {
     savings: false
 };
 
-let q1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let q2 = prompt("Во сколько обойдется?", "");
-let q3 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let q4 = prompt("Во сколько обойдется?", "");
 
-appData.expenses.q1 = q2;
-appData.expenses.q3 = q4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = prompt("Во сколько обойдется?", "");
 
-alert(appData.budget / 30);
+    if (typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    }
+    else {
+        alert('Ошибка 1');
+        i--;
+    }
+}
+
+/*  let i = 0;
+    while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = prompt("Во сколько обойдется?", "");
+
+    if (typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    }
+    else {
+        alert('Ошибка 1');
+        i--;
+    }
+    i++;
+} */
+
+/*  let i = 0;
+    do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = prompt("Во сколько обойдется?", "");
+
+    if (typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    }
+    else {
+        alert('Ошибка 1');
+        i--;
+    }
+    i++;
+} while (i < 2); */
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    alert('Минимальный уровень достатка');
+}
+else if (appData.moneyPerDay < 2000) {
+    alert('Средний уровень достатка');
+}
+else if (appData.moneyPerDay >= 2000) {
+    alert('Высокий уровень достатка');
+}
+else {
+    alert('Ошибка 2');
+}
